c135e013ac5c5ec8d41cd1c058b3a311c6d5bb62
Alberto97 <albertop2197@gmail.com>
2013-12-21 22:28:00 +0100

    Add some echo
    Change-Id: Ica8855b0794a33bb3827f4674daa36560cbccda3

--- build.sh
+++ build.sh
@@ -2,11 +2,43 @@
 NBR="12"
 
 # Do not edit!
-echo "Rom Building Started"
+echo ""
+echo ""
+echo "       ############################################################### "
+echo "      #                       ________   _________  ________          #"
+echo "      #              /\      |        | |          |        |         #"
+echo "      #             /  \     |        | |          |        |         #"
+echo "      #            /    \    |        | |________  |________|         #"
+echo "      #           /______\   |        |          | |                  #"
+echo "      #          /        \  |        |          | |                  #"
+echo "      #         /          \ |________| _________| |                  #"
+echo "      #                                                               #"
+echo "       ############################################################### "
+echo ""
+echo "       ############################################################### "
+echo "      #    _________  _________  _________               __    __     #"
+echo "      #        |          |     |               /\      |  \  /  |    #"
+echo "      #        |          |     |              /  \     |   \/   |    #"
+echo "      #        |   ____   |     |____         /    \    |        |    #"
+echo "      #        |          |     |            /______\   |        |    #"
+echo "      #        |          |     |           /        \  |        |    #"
+echo "      #     ___|          |     |_________ /          \ |        |    #"
+echo "      #                                                               #"
+echo "       ############################################################### "
+echo ""
+echo ""
 . patches/patch.sh
 . build/envsetup.sh
 lunch full_janice-userdebug
 STARTTIME=$SECONDS
 make otapackage -j$NBR 
 ENDTIME=$SECONDS
-echo -e "\n\n = Finished in $((ENDTIME-STARTTIME)) Seconds =\n\n"
+echo "       ############################################################### "
+echo "      #                                                               #"
+echo "      #                                                               #"
+echo "      #                                                               #"
+echo -e "                        = Finished in $((ENDTIME-STARTTIME)) Seconds ="
+echo "      #                                                               #"
+echo "      #                                                               #"
+echo "      #                                                               #"
+echo "       ############################################################### "
